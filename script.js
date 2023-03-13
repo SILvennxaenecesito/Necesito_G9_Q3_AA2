ffde371da658d2c76fcf797a86c6ddc326ac3aa9
SILvennxaenecesito <126746668+SILvennxaenecesito@users.noreply.github.com>
2023-03-13 10:57:39 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+//1.
+
+var x = 2;
+while(x<61){
+	document.writeln(x);
+	x=x+2;
+}
+
+//2.
+var y = 3;
+do{
+	document.writeln(y);
+	y = y+3;
+}
+while(y<61)
+
+//3. 
+
+for(z=5;z<51;z=z+5){
+	document.writeln(z);
+}
